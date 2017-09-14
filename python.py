e908c8eff168e3c20f2e93939429b39e2d66676f
Phyllis Wong <wong.phyllism@gmail.com>
2017-09-14 13:13:33 -0700

    revised fizzbuzz

--- python.py
+++ python.py
@@ -2,25 +2,22 @@
 def fizz_buzz_sums(num):
     sum = 0
     for i in range(num):
-        if i % 15 == 0:
+        if (i % 3 == 0) or (i % 5 == 0):
             sum += i
-        elif i % 3 == 0:
-            sum += i
-        elif i % 5 == 0:
-            sum += i
-return sum
+    return sum
 
 fizz_buzz_sums(1000)
 
+# project Euler Challenge 2
 def fibonoci_sum(num):
     f_sum = 0
     cur = 0
     f_next = 1
     temp = 0
     while cur < num:
-        cur = f_next + temp
+        cur = (f_next + temp)
         temp = f_next
         f_next = cur
-        if cur % 2 == 0:
+        if (cur % 2 == 0):
             f_sum += cur
     print(f_sum)
