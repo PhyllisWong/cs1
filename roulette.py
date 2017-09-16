8e4cc94a55f175acd1e083c9b5fd9d3da8c4ab3d
Phyllis Wong <wong.phyllism@gmail.com>
2017-09-16 13:56:07 -0700

    fixed roll_ball function

--- roulette.py
+++ roulette.py
@@ -18,18 +18,29 @@ bet_amount = 0
 bet_color = None
 bet_num = None
 
-gren = [0, 37]
+green = [0, 37]
 red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 
 def take_bet(color, number, amount):
-    bet_color = color
-    bet_number = number
-    bet_amount = amount
+    total_bet = []
+    total_bet.append(color)
+    total_bet.append(number)
+    total_bet.append(amount)
+    return total_bet
 
 def roll_ball():
     '''returns a random number between 0 and 37'''
-    pass
+    results = []
+    ball_num = random.randint(0, 37)
+    if (ball_num in green):
+        results.append('green')
+    elif (ball_num in red):
+        results.append('red')
+    elif (ball_num in black):
+        results.append('black')
+    results.append(ball_num)
+    return results
 
 def check_results():
     '''Compares bet_color to color rolled.  Compares
