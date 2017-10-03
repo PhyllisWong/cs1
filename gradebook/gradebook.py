91008d5af219f81778b990635841d4feb1f45452
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-03 11:54:18 -0700

    added assignment class

--- gradebook/gradebook.py
+++ gradebook/gradebook.py
@@ -3,7 +3,9 @@
 
 class Classroom:
     """Classroom instantiated with name of class and teacher."""
+
     def __init__(self, class_name, teacher):
+        """Initialize the class with class name and teacher name."""
         self.class_name = class_name
         self.teacher = teacher
 
@@ -13,22 +15,23 @@ def get_student_roster():
     pass
 
 
-def get_total_grades_average():
-    """Method to claculate the average for the whole class."""
-    pass
-
-
 class Student:
     """Information about each student."""
+
     def __inti__(self, name):
+        """Initializing student name."""
         self.name = name
 
     assignments = {}
     # x is placeholder for the weight of each assignment
-    assignment[x] = {}
+    assignments[x] = {}
     # id is the student unique id
     # grade is the grade earned on the assignment
-    assignment[x][id] = grade
+    assignments[x][id] = grade
+
+    def get_total_grades_average():
+        """Method to claculate the average for the whole class."""
+        pass
 
     def create_student_id():
         """Method to assign each student a unique student ID."""
@@ -37,3 +40,38 @@ class Student:
     def get_student_attendance():
         """Method to store attendance record in student object."""
         pass
+
+
+class Assignment:
+    """Information about each assignment"""
+
+    homeworks = []
+    quizzes = []
+    exams = []
+
+    def __init__(self, assignment_name, assignment_type):
+        """Initialize assignment name and the type."""
+        self.assignment_name = assignment_name
+        self.assignment_type = assignment_type
+
+    def get_assignment_weight(assignment_type):
+        """Take in a string argument, returns a float."""
+        a = assignment_type
+        weight = None
+        if a == "homework":
+            weight = 0.5
+        elif a == "quiz":
+            weight = 0.3
+        elif a == "homework":
+            weight = 0.2
+        return weight
+
+    def get_average_of_type(type_arr):
+        """Take in array, return an Int."""
+        type_sum = None
+
+        for each in type_arr:
+            type_sum += each
+
+        quotient = type_sum / len(type_sum)
+        return quotient
