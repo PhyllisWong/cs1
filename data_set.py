20a7bac6e05eba6910b7b4860a60906fe8acc095
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-11 16:08:38 -0700

    completed challenge 2, part 1

--- data_set.py
+++ data_set.py
@@ -1,8 +1,7 @@
 """Data scraping using Python String Methods."""
 """
 Questions for today’s File I/O problem:
-1. What are the total amount of sales contained in this data set?
-2. Which city had the highest sales in February?
+
 3. Out of the entire data set, what is the total amount of money
     people have paid in cash?
 4. What is the most popular payment type in Oakland in March?
@@ -11,9 +10,12 @@ Questions for today’s File I/O problem:
 7. How many purchases were made by bartering with baseball cards?
 """
 
-"""~~~~~~~~~~~~~~~~  Challenge 1  ~~~~~~~~~~~~~~~"""
+"""
+~~~~~~~~~~~~~~~~  Challenge 1  ~~~~~~~~~~~~~~~
+1. What are the total amount of sales contained in this data set?
+"""
 # Read the file in python
-with open("sales_data.txt") as f:
+with open("sales_data.txt", "r") as f:
     data = list(f)
     count = 0
     total = 0
@@ -25,7 +27,19 @@ with open("sales_data.txt") as f:
         total += float(cell[3].strip("$\n"))
     # limit the decimals places to 2
     result = str(round(total, 2))
-    print(result)
+    # print(result)
 # Answer is 500143918.36
 
-"""~~~~~~~~~~~~~~~~  Challenge 2  ~~~~~~~~~~~~~~~"""
+"""
+~~~~~~~~~~~~~~~~  Challenge 2  ~~~~~~~~~~~~~~~
+2. Which city had the highest sales in February?
+"""
+# Open file, read each line, split on each "tab"
+with open("sales_data.txt", "r") as f:
+    data_array = []
+    data_dictionary = {}
+    for word in f.readline().split('\t'):
+        data_array.append(word.strip('$\n'))
+        # iterate thru the array
+        # if array[0] !in dictionary, create a new key with value 
+    print(data_array)
