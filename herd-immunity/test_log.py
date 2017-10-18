195e43b98edaa650b1ab414398f99ad88a413e30
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-17 22:00:28 -0700

    finished write metadata func

--- herd-immunity/test_log.py
+++ herd-immunity/test_log.py
@@ -1,5 +1,6 @@
 import pytest
 from logger import Logger
+from person import Person
 import os
 """Check to see if log outputs as expected."""
 
@@ -12,14 +13,25 @@ def setup_for_test():
 
 
 def test_write_metadata():
-    # setup_for_test()
-    # f = open("./logs/logging.txt", "r")
-    # line_one = f.readline()
-    # f.close()
-    Logger.write_metadata("100", "0.5", "HIV", "0.5", "0.1")
-    # print(line_one)
-    assert line_one == "100\t0.5\tHIV\t0.5\t0.1"
+    log1 = Logger("log1.txt")
+    metadata = log1.write_metadata("100", "0.5", "HIV", "0.5", "0.1")
+    print(log1)
+    assert metadata == "100\t0.5\tHIV\t0.5\t0.1\n"
 
 
-def test_interactions():
+'''
+log_interaction(self, person1, person2, did_infect=None,
+                    person2_vacc=None, person2_sick=None)
+'''
+
+# TODO: The Simulation object logs each interaction a sick individual
+# has during each time step.
+# This method should accomplish this by using the information
+# from person1 (the infected person), person2 (the person randomly
+# chosen for the interaction), & the optional keyword arguments passed
+# into the method. See documentation for info on the format of logs
+#  this method writes.
+
+
+def test_log_interaction():
     pass
