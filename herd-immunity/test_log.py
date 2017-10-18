4aec46bc7c143417d36a17283cb03b2d44623a75
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-17 17:55:40 -0700

    metaData function

--- herd-immunity/test_log.py
+++ herd-immunity/test_log.py
@@ -1,18 +1,24 @@
 import pytest
-import logger
+from logger import Logger
 import os
 """Check to see if log outputs as expected."""
 
 
-def test_write_metadata():
+def setup_for_test():
     f = open("./logs/logging.txt", "w")
     f.write("Testing Logs Yo!")
     f.close()
-    f = open("./logs/logging.txt", "r")
-    line_one = f.readline()
-    f.close()
-    print(line_one)
-    assert line_one == "Testing Logs Yo!"
+    return f
+
+
+def test_write_metadata():
+    # setup_for_test()
+    # f = open("./logs/logging.txt", "r")
+    # line_one = f.readline()
+    # f.close()
+    Logger.write_metadata("100", "0.5", "HIV", "0.5", "0.1")
+    # print(line_one)
+    assert line_one == "100\t0.5\tHIV\t0.5\t0.1"
 
 
 def test_interactions():
